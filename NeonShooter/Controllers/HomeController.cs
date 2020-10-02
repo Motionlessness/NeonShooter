91237b141f23c77e05afa6b12361ed290650a25f
Wil Crouch <william.crouch@hotmail.com>
2020-10-01 18:59:59 -0700

    tied up links to new page

--- NeonShooter/Controllers/HomeController.cs
+++ NeonShooter/Controllers/HomeController.cs
@@ -28,6 +28,11 @@ namespace NeonShooter.Controllers
             return View();
         }
 
+        public IActionResult NeonShooter()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
